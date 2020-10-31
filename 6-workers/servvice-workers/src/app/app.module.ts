31001302cb728b977e34d48637df8c4ff8b89731
Stepan Suvorov <stevermeister@gmail.com>
2020-10-31 16:09:47 +0100

    workers

--- 6-workers/servvice-workers/src/app/app.module.ts
+++ 6-workers/servvice-workers/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- questions.md
+++ questions.md
@@ -3,3 +3,4 @@
 
 1. Angular Elements: А как-то можно типизировать CustomEvent, который прилетает при клике?
 2. Angular Elements: как передать динамически ng-content (не innerHTML)
+3. Service Workers: А можно service worker использовать без добавления pwa? (нужны апдейты, но не нужен офлайн)
